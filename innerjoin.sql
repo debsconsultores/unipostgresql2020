1a26a78f65d6649da610b2f762f0f6fceb1bd61c
debsdaniel <dbojorges@gmail.com>
2020-03-10 19:24:12 -0600

    20200310-1923 SQL Inner Join

---
+++ innerjoin.sql
@@ -0,0 +1,17 @@
+--Primer Inner Join
+select customer.customer_id,customer.first_name,customer.last_name,
+	payment.amount
+	from customer 
+	inner join payment on customer.customer_id=payment.customer_id
+
+--Alias a las tablas
+select c.customer_id,c.first_name,c.last_name,
+	p.amount
+	from customer as c
+	inner join payment as p on c.customer_id=p.customer_id
+	
+--Alias a los campos
+select c.customer_id ,c.first_name,c.last_name,
+	p.amount
+	from customer as c
+	inner join payment as p on c.customer_id=p.customer_id
