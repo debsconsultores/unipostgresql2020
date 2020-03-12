55f4100bf96d1f34f484923edc3851460cb591bc
debsdaniel <dbojorges@gmail.com>
2020-03-11 19:26:09 -0600

    20200311-1822 Inner Join - 1

--- innerjoin.sql
+++ innerjoin.sql
@@ -15,3 +15,18 @@ select c.customer_id as Id ,c.first_name as nombres,c.last_name as apellidos,
 	p.amount as monto
 	from customer as c
 	inner join payment as p on c.customer_id=p.customer_id
+	
+--Inner Join Where
+select c.customer_id as Id ,c.first_name as nombres,c.last_name as apellidos,
+	p.amount as monto
+	from customer as c
+	inner join payment as p on c.customer_id=p.customer_id
+	where c.customer_id = 354
+	
+--Inner Join Where y Order
+select c.customer_id as Id ,c.first_name as nombres,c.last_name as apellidos,
+	p.amount as monto
+	from customer as c
+	inner join payment as p on c.customer_id=p.customer_id
+	where c.customer_id = 354 order by p.amount DESC
+	
