3c85c303c6950e85da1e7a30a677c691c22d4f69
debsdaniel <dbojorges@gmail.com>
2020-03-17 19:24:10 -0600

    20200317-1924 CTE - 2

--- cte.sql
+++ cte.sql
@@ -29,11 +29,28 @@ select
 )
 select s.staff_id,s.first_name,s.last_name,cs.conteo
 	from staff s inner join cte_staff as cs on (s.staff_id=cs.staff_id)
+	
+with pago_cliente as (
+select 
+	r.customer_id,
+	sum(p.amount) as pagado
+	from rental as r
+	inner join payment as p on r.rental_id=p.rental_id
+	group by r.customer_id
+)
+select p.customer_id,c.first_name,c.last_name,p.pagado
+	from customer c inner join pago_cliente p on c.customer_id=p.customer_id
 
-
-
-
-
+with cte_payment_1 as (
+select * from payment 
+	where customer_id=107 
+	and payment_date::date between '20070220' and '20070228'
+),cte_payment_2 as(
+select * from payment 
+	where customer_id=107 
+	and payment_date::date between '20070220' and '20070310'
+)
+select count(*) from cte_payment_1 union all select count(*) from cte_payment_2
 
 
 
