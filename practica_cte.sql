0ba77842e22e8ade6cfc5e8eee94aec6a5ac4fe7
debsdaniel <dbojorges@gmail.com>
2020-03-18 19:04:03 -0600

    20200318-1903 Pratica CTE - 2

--- practica_cte.sql
+++ practica_cte.sql
@@ -28,6 +28,27 @@ insert into ventas (producto_id,cant,precio) values
 	(1,50,20),(2,15,50),(4,1,55000)
 select * from ventas
 
+with cmp(pc_id,total_cmp) as (
+	select producto_id,sum(cant*precio) from compras group by producto_id
+),vta(pv_id,total_vta) as(
+	select producto_id,sum(cant*precio) from ventas group by producto_id
+),total_cmp(total) as (
+	select sum(total_cmp) from cmp 
+)
+select 
+	p.id,p.nombre,c.total_cmp as compras,v.total_vta as ventas
+	from productos as p
+	left join cmp as c on c.pc_id=p.id
+	left join vta as v on v.pv_id=p.id
+union all
+	select null,'Total Compras',tc.total,null from total_cmp as tc
+
+
+
+
+
+
+
 
 
 
