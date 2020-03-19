2f64781779370a18ebb7021b1ad45857ff1ad345
debsdaniel <dbojorges@gmail.com>
2020-03-19 17:54:36 -0600

    20200319-1754 Pratica CTE - 3 - Modificar Registros

---
+++ cte_modificacion.sql
@@ -0,0 +1,34 @@
+CREATE TABLE producto_2(
+	id serial not null primary key,
+	nombre varchar(50) not null
+);
+
+create table producto_log(
+	id int,
+	nombre varchar(50),
+	fecha_cambio timestamp default now()
+);
+
+insert into producto_2 (nombre) values
+('Mandarina'),('Agua'),('Laptop'),('TV'),('Celular');
+select * from producto_2;
+select * from producto_2 where nombre ilike '%v'
+
+with prod_elim as (
+	delete from producto_2 where nombre ilike '%lar'
+	--returning *
+)
+insert into producto_log (id,nombre)
+select * from prod_elim
+
+select * from producto_log
+select * from producto_2 where nombre ilike '%lar'
+
+
+
+
+
+
+
+
+
