df27c5f3b037e1c785ebdd062663771eba390800
debsdaniel <dbojorges@gmail.com>
2020-03-24 18:34:50 -0600

    20200324-1823 Indices Unique

--- indices_unique.sql
+++ indices_unique.sql
@@ -18,3 +18,16 @@ CREATE UNIQUE INDEX idx_emp_tel ON EMPLOYEE(TELEFONO)
 INSERT INTO EMPLOYEE (first_name,last_name,email,telefono) values ('Pedro','Perez','pedro@uni.edu.ni',22664532)
 INSERT INTO EMPLOYEE (first_name,last_name,email,telefono) values ('Juan','Lopez','pedro@uni.edu.ni',22664532)  --FALLA
 INSERT INTO EMPLOYEE (first_name,last_name,email,telefono) values ('Juan','Lopez','juan@uni.edu.ni',22664532)
+
+ALTER TABLE EMPLOYEE ADD telefono_oficina varchar(20), ADD ext varchar(5);
+CREATE UNIQUE INDEX idx_emp_tel_of_ext ON employee(telefono_oficina,ext)
+
+INSERT INTO EMPLOYEE (first_name,last_name,email,telefono,telefono_oficina,ext)
+	values ('Juan','Perez','juanp@uni.edu.ni','123456','123456','110')
+INSERT INTO EMPLOYEE (first_name,last_name,email,telefono,telefono_oficina,ext)
+	values ('Juan','Lopez','juan@uni.edu.ni','654321','123456','112')
+INSERT INTO EMPLOYEE (first_name,last_name,email,telefono,telefono_oficina,ext)
+	values ('Pedro','Lopez','pedrol@uni.edu.ni','254654321','123456','112')
+INSERT INTO EMPLOYEE (first_name,last_name,email,telefono,telefono_oficina,ext)
+	values ('Pedro','Lopez','pedrol@uni.edu.ni','254654321','123456','111')
+select * from employee
